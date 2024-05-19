edf487dd4a690bc58e3310c87cbe64ad8d6738f1
redaelhoumadi <reda.elhoumadi@gmail.com>
2024-05-19 16:14:51 +0200

    add utils

---
+++ Front/src/utils/assets.js
@@ -0,0 +1,7 @@
+import logo from "@/assets/images/argentBankLogo.png";
+import bankTree from "../assets/images/bank-tree.jpeg";
+import chat from "../assets/images/icon-chat.png";
+import money from "../assets/images/icon-money.png";
+import security from "../assets/images/icon-security.png";
+
+export { logo, bankTree, chat, money, security };
