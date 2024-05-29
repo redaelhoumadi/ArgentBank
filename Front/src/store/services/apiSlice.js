4c4846f56e556ac0daf7fbad7c236e238abce422
redaelhoumadi <reda.elhoumadi@gmail.com>
2024-05-29 17:56:45 +0200

    Edit Transaction & comment code

--- Front/src/store/services/apiSlice.js
+++ Front/src/store/services/apiSlice.js
@@ -1,23 +1,34 @@
+// Importation de BASE_URL à partir de la configuration
 import { BASE_URL } from "@/config";
+
+// Importation des fonctions nécessaires de Redux Toolkit Query
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Importation de la bibliothèque js-cookie pour gérer les cookies
 import Cookies from "js-cookie";
 
+// Fonction pour obtenir le jeton d'accès à partir des cookies
 const getAccessToken = () => Cookies.get("access_token") || "";
 
+// Configuration de la baseQuery avec fetchBaseQuery
 const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
+  baseUrl: BASE_URL,  // URL de base pour toutes les requêtes
   prepareHeaders: (headers) => {
+    // Obtention du jeton d'accès
     const access_token = getAccessToken();
 
+    // Si un jeton d'accès est présent, l'ajouter aux en-têtes
     if (access_token) {
       headers.set("Authorization", `Bearer ${access_token}`);
     }
+    // Définir le type de contenu à JSON pour toutes les requêtes
     headers.set("Content-Type", "application/json");
-    return headers;
+    return headers;  // Retourner les en-têtes modifiés
   },
 });
 
+// Création de l'apiSlice avec createApi de Redux Toolkit Query
 export const apiSlice = createApi({
-  baseQuery,
-  endpoints: () => ({}),
+  baseQuery,  // Utiliser baseQuery comme configuration de base
+  endpoints: () => ({}),  // Initialiser avec des endpoints vides, à compléter plus tard
 });
--- Front/src/store/services/authApiSlice.js
+++ Front/src/store/services/authApiSlice.js
@@ -1,47 +1,57 @@
 import { apiSlice } from "./apiSlice";
 
+// Définition de l'URL de base pour les utilisateurs
 const USER_URL = "/user";
 
+// Amélioration de l'apiSlice en ajoutant un type de tag "user"
 const apiSliceWithTag = apiSlice.enhanceEndpoints({
-  addTagTypes: ["user"],
+  addTagTypes: ["user"],  // Ajout du tag "user" pour le cache et la gestion des données
 });
 
+// Définition et exportation des endpoints pour les opérations d'authentification
 export const authApiSlice = apiSliceWithTag.injectEndpoints({
   endpoints: (builder) => ({
+
+    // Endpoint pour la mutation de connexion
     login: builder.mutation({
       query: (data) => {
         return {
-          url: `${USER_URL}/login`,
-          method: "POST",
-          body: data,
+          url: `${USER_URL}/login`,  // URL pour la requête de connexion
+          method: "POST",  // Méthode HTTP POST
+          body: data,  // Corps de la requête contenant les données de connexion
         };
       },
     }),
+
+    // Endpoint pour la requête de l'utilisateur actuel
     getCurrentUser: builder.query({
       query: () => {
         return {
-          url: `${USER_URL}/profile`,
-          method: "POST",
-          body: {},
+          url: `${USER_URL}/profile`,  // URL pour récupérer le profil de l'utilisateur actuel
+          method: "POST",  // Méthode HTTP POST
+          body: {},  // Corps vide
         };
       },
-      providesTags: ["user"],
+      providesTags: ["user"],  // Tag "user" fourni pour la gestion du cache
     }),
+    
+    // Endpoint pour la mutation de mise à jour du profil
     updateProfile: builder.mutation({
       query: (data) => {
         return {
-          url: `${USER_URL}/profile`,
-          method: "PUT",
-          body: data,
+          url: `${USER_URL}/profile`,  // URL pour mettre à jour le profil de l'utilisateur
+          method: "PUT",  // Méthode HTTP PUT
+          body: data,  // Corps de la requête contenant les données de mise à jour
         };
       },
-      invalidatesTags: ["user"],
+      invalidatesTags: ["user"],  // Invalidation du tag "user" pour rafraîchir les données mises en cache
     }),
   }),
 });
 
+// Exportation des hooks générés pour utiliser les mutations et requêtes dans les composants
 export const {
-  useLoginMutation,
-  useGetCurrentUserQuery,
-  useUpdateProfileMutation,
+  useLoginMutation,  // Hook pour la mutation de connexion
+  useGetCurrentUserQuery,  // Hook pour la requête de l'utilisateur actuel
+  useUpdateProfileMutation,  // Hook pour la mutation de mise à jour du profil
 } = authApiSlice;
